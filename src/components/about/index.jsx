d97e07552464eaa870c5c14692c9731d2057e0e7
Léopold Assogba <leopoldassogba21@gmail.com>
2020-10-13 01:04:20 +0200

    add about dev

--- src/components/about/index.jsx
+++ src/components/about/index.jsx
@@ -1,6 +1,6 @@
 import { Button, Col, Row } from "antd";
-import React, { useEffect, useRef } from "react";
-import gsap, { TimelineLite, Power2, Bounce } from "gsap";
+import React, { useEffect, useRef, useState } from "react";
+import gsap, { TimelineLite, Power2, Power1, Bounce } from "gsap";
 import CSSRulePlugin from "gsap/CSSRulePlugin";
 
 import { RightCircleOutlined } from "@ant-design/icons";
@@ -14,92 +14,218 @@ const About = () => {
   let laptopContainerRef = useRef(null);
   let laptopRevealRef = useRef(null);
   let laptopImgRef = useRef(null);
+  let lightRef = useRef(null);
+  let devTextContainer = useRef(null);
+  let laptop2ImgRef = useRef(null);
+
+  const [section, setSection] = useState(0);
+  const sections = ["abstract", "dev", "music"];
 
   useEffect(() => {
-    gsap.from(".light", {
-      duration: 1.6,
-      opacity: 0,
-      ease: Bounce.easeInOut,
-      delay: 0.8,
-    });
-    gsap.from(".abstract", {
-      duration: 1.6,
-      opacity: 0,
-      ease: Bounce.easeInOut,
-      delay: 0.8,
-    });
-    gsap.to(laptopRevealRef, {
-      duration: 1.6,
-      height: 0,
-      ease: Power2.easeInOut,
-      delay: 1.2,
-    });
+    if (section === 0) {
+      gsap.from(".light", {
+        duration: 1.6,
+        opacity: 0,
+        ease: Bounce.easeInOut,
+        delay: 0.8,
+      });
+      gsap.from(".abstract", {
+        duration: 1.6,
+        opacity: 0,
+        ease: Bounce.easeInOut,
+        delay: 0.8,
+      });
+      gsap.to(laptopRevealRef, {
+        duration: 1.6,
+        height: 0,
+        ease: Power2.easeInOut,
+        delay: 1.2,
+      });
+    }
   }, []);
 
+  useEffect(() => {
+    if (section === 1) {
+      gsap.to(lightRef, {
+        duration: 1,
+        opacity: 1,
+        ease: Bounce.easeInOut,
+      });
+      gsap.to(devTextContainer, {
+        duration: 1,
+        opacity: 1,
+        ease: Bounce.easeInOut,
+      });
+      gsap.to(laptop2ImgRef, {
+        duration: 1,
+        opacity: 1,
+        left: 0,
+        ease: Power1.easeIn,
+        delay: 1,
+      });
+    }
+  }, [section]);
+
   const goToNextContent = () => {
-    gsap.to(".light", {
-      duration: 1,
-      opacity: 0,
-      ease: Power2.easeInOut,
-    });
-    gsap.to(".abstract", {
-      duration: 1,
-      opacity: 0,
-      ease: Power2.easeOut,
-    });
-    gsap.to(laptopImgRef, {
-      duration: 1,
-      position: "absolute",
-      left: "-20%",
-      opacity: 0,
-      delay: 1,
-      ease: Power2.easeOut,
-    });
+    if (section === 0) {
+      gsap.to(".light", {
+        duration: 1,
+        opacity: 0,
+        ease: Power2.easeInOut,
+      });
+      gsap.to(".abstract", {
+        duration: 1,
+        opacity: 0,
+        ease: Power2.easeOut,
+      });
+      gsap.to(laptopImgRef, {
+        duration: 1,
+        left: "-20%",
+        opacity: 0,
+        delay: 1,
+        ease: Power2.easeOut,
+      });
+      gsap.to(laptopImgRef, {
+        duration: 0,
+        visibility: "hidden",
+        delay: 2,
+        ease: Power2.easeOut,
+      });
+      setTimeout(() => {
+        setSection((state) => (state += 1));
+      }, 2000);
+    } else if (section === 1) {
+      gsap.to(lightRef, {
+        duration: 1,
+        opacity: 0,
+        ease: Bounce.easeInOut,
+      });
+      gsap.to(devTextContainer, {
+        duration: 1,
+        opacity: 0,
+        ease: Bounce.easeInOut,
+      });
+      gsap.to(laptop2ImgRef, {
+        duration: 1,
+        opacity: 0,
+        left: "-20%",
+        ease: Power2.easeOut,
+        delay: 1,
+      });
+
+      setTimeout(() => {
+        setSection((state) => (state += 1));
+      }, 2000);
+    }
   };
 
+  console.log(sections[section]);
   return (
     <div className="aboutContainer">
-      <div className="abstractContainer">
-        <Row>
-          <Col span={24} offset={1}>
-            <div className="light"></div>
-          </Col>
-        </Row>
-        <Row>
-          <Col span={16} offset={4}>
-            <div className="abstract">
-              <h2>Léopold Assogba</h2>
-              <p>Hello, welcome to my portfolio..</p>
-              <p>
-                I would like to share with you two of my passions: web
-                application development and music.
-              </p>
-              <p>
-                <Button
-                  type="link"
-                  shape="round"
-                  onClick={() => goToNextContent()}
+      {sections[section] === "abstract" && (
+        <div className="abstractContainer">
+          <Row>
+            <Col span={24} offset={1}>
+              <div className="light"></div>
+            </Col>
+          </Row>
+          <Row>
+            <Col span={16} offset={4}>
+              <div className="abstract">
+                <h2>Léopold Assogba</h2>
+                <p>Hello, welcome to my portfolio..</p>
+                <p>
+                  I would like to share with you two of my passions: web
+                  application development and music.
+                </p>
+                <p>
+                  <Button
+                    type="link"
+                    shape="round"
+                    onClick={() => goToNextContent()}
+                  >
+                    More
+                    <RightCircleOutlined />
+                  </Button>
+                </p>
+              </div>
+            </Col>
+          </Row>
+          <Row>
+            <Col span={24}>
+              <div className="laptop" ref={(el) => (laptopContainerRef = el)}>
+                <div ref={(el) => (laptopRevealRef = el)}></div>
+                <img
+                  src="assets/img/laptop.jpg"
+                  alt="laptop"
+                  ref={(el) => (laptopImgRef = el)}
+                />
+              </div>
+            </Col>
+          </Row>
+        </div>
+      )}
+      {sections[section] === "dev" && (
+        <div className="aboutDevContainer">
+          <Row>
+            <Col span={8}>
+              <div className="laptop2Container">
+                <img
+                  src="assets/img/laptop2.jpg"
+                  alt="laptop 2"
+                  ref={(el) => (laptop2ImgRef = el)}
+                />
+              </div>
+            </Col>
+            <Col span={16}>
+              <div className="aboutDev">
+                <div className="lightContainer">
+                  <img
+                    src="assets/img/light2.jpg"
+                    alt="leaf "
+                    ref={(el) => (lightRef = el)}
+                  />
+                </div>
+                <div
+                  className="devTextContainer"
+                  ref={(el) => (devTextContainer = el)}
                 >
-                  More
-                  <RightCircleOutlined />
-                </Button>
-              </p>
-            </div>
-          </Col>
-        </Row>
-        <Row>
-          <Col span={24}>
-            <div className="laptop" ref={(el) => (laptopContainerRef = el)}>
-              <div ref={(el) => (laptopRevealRef = el)}></div>
-              <img
-                src="assets/img/laptop.jpg"
-                alt="laptop"
-                ref={(el) => (laptopImgRef = el)}
-              />
-            </div>
-          </Col>
-        </Row>
-      </div>
+                  <div>
+                    <h2>About developpement</h2>
+                    <p>
+                      Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                      sed do eiusmod tempor incididunt ut labore et dolore magna
+                      aliqua. Ut enim ad minim veniam, quis nostrud exercitation
+                      ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                      Duis aute irure dolor in reprehenderit in voluptate velit
+                      esse cillum dolore eu fugiat nulla pariatur. Excepteur
+                      sint occaecat cupidatat non proident, sunt in culpa qui
+                      officia deserunt mollit anim id est laborum.
+                    </p>
+                    <Button
+                      type="link"
+                      shape="round"
+                      onClick={() => goToNextContent()}
+                    >
+                      More
+                      <RightCircleOutlined />
+                    </Button>
+                  </div>
+                </div>
+              </div>
+            </Col>
+          </Row>
+        </div>
+      )}
+      {sections[section] === "music" && (
+        <div className="aboutDevContainer">
+          <img src="assets/img/piano.jpg" alt="piano" />
+          {/* TODO: position: absolute;
+    width: 51%;
+    bottom: 0;
+    right: 0; */}
+        </div>
+      )}
     </div>
   );
 };
