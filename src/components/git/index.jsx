8e9913e24458d14cdb3c9bd9c81231cb1b98597c
Léopold Assogba <leopoldassogba21@gmail.com>
2020-10-14 21:40:25 +0200

    todo display readme

--- src/components/git/index.jsx
+++ src/components/git/index.jsx
@@ -8,22 +8,23 @@ const GitComponent = () => {
     auth: "5483e014842e8d8bd6fd75f12e90aab3401aee2a",
   });
 
-  const loadRepos = async () => {
-    const owner = "LeopoldAssogbaA";
-
-    const fetchRepos = await octokit.request(`GET /users/{owner}/repos`, {
-      owner,
-    });
-    setRepos(fetchRepos.data);
-    setRepoLoaded(true);
-  };
-
   useEffect(() => {
+    const loadRepos = async () => {
+      const owner = "LeopoldAssogbaA";
+
+      const fetchRepos = await octokit.request(`GET /users/{owner}/reposR`, {
+        owner,
+      });
+      setRepos(fetchRepos.data);
+      setRepoLoaded(true);
+    };
+
     !repoLoaded && loadRepos();
-  }, [repoLoaded]);
+  }, [octokit, repoLoaded]);
 
   console.log(repos[0]);
-
+  // TODO: get readme
+  // https://developer.github.com/v3/repos/contents/#get-a-repository-readme
   return (
     <div>
       <h2>Github Repositories</h2>
@@ -37,6 +38,10 @@ const GitComponent = () => {
                 <span>{repo.created_at}</span>
                 <br />
                 <span>{repo.description}</span>
+                <br />
+                <span>{repo.size} Mo</span>
+                <br />
+                <a href={repo.html_url}>Go to repo page </a>
                 <br />
               </div>
             </li>
