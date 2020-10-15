24a61aed8a61a3c9fe909ff16df154ad216b85e3
Léopold Assogba <leopoldassogba21@gmail.com>
2020-10-15 17:15:23 +0200

    get readmes and format html content

--- src/components/git/index.jsx
+++ src/components/git/index.jsx
@@ -1,9 +1,15 @@
 import React, { useEffect, useState } from "react";
 import { Octokit } from "@octokit/core";
+import { LoadingOutlined } from "@ant-design/icons";
+
+import "./index.less";
 
 const GitComponent = () => {
   const [repos, setRepos] = useState([]);
-  const [repoLoaded, setRepoLoaded] = useState(false);
+  const [reposLoaded, setReposLoaded] = useState(false);
+  const [reposUpdated, setReposUpdated] = useState(false);
+  const [readmesLoaded, setReadmesLoaded] = useState(false);
+
   const octokit = new Octokit({
     auth: "5483e014842e8d8bd6fd75f12e90aab3401aee2a",
   });
@@ -11,41 +17,96 @@ const GitComponent = () => {
   useEffect(() => {
     const loadRepos = async () => {
       const owner = "LeopoldAssogbaA";
-
-      const fetchRepos = await octokit.request(`GET /users/{owner}/reposR`, {
-        owner,
-      });
-      setRepos(fetchRepos.data);
-      setRepoLoaded(true);
+      try {
+        const fetchRepos = await octokit.request(`GET /users/{owner}/repos`, {
+          owner,
+        });
+        setRepos(fetchRepos.data);
+        setReposLoaded(true);
+      } catch (e) {
+        console.error("error repos fetch", e);
+      }
     };
 
-    !repoLoaded && loadRepos();
-  }, [octokit, repoLoaded]);
+    const loadReadmes = () => {
+      const owner = "LeopoldAssogbaA";
+      const reposUpdated = [];
 
-  console.log(repos[0]);
-  // TODO: get readme
-  // https://developer.github.com/v3/repos/contents/#get-a-repository-readme
+      try {
+        repos.forEach(async (repo) => {
+          const repoName = repo.name;
+          const fetchReadme = await octokit.request(
+            `GET /repos/{owner}/{repoName}/readme`,
+            {
+              owner,
+              repoName,
+              headers: { Accept: "application/vnd.github.html" },
+            }
+          );
+
+          const currentRepoIndex = repos.findIndex(
+            (repo) => repoName === repo.name
+          );
+
+          reposUpdated.push({
+            ...repos[currentRepoIndex],
+            readme_html: fetchReadme.data,
+          });
+          if (reposUpdated.length === repos.length) {
+            setRepos(reposUpdated);
+            console.log("reposUpdated!!!");
+            setReposUpdated(true);
+          }
+        });
+        setReadmesLoaded(true);
+      } catch (e) {
+        console.error("error", e);
+      }
+    };
+
+    const regex = 'src="/LeopoldAssogbaA/"';
+    console.log(regex);
+
+    !reposLoaded && loadRepos();
+    reposLoaded && !readmesLoaded && loadReadmes();
+  }, [octokit, readmesLoaded, repos, reposLoaded]);
+
   return (
-    <div>
+    <div className="gitContainer">
       <h2>Github Repositories</h2>
-      <ul>
-        {repoLoaded &&
-          repos.map((repo) => (
-            <li key={repo.id}>
-              <div>
-                <span>{repo.name}</span>
-                <br />
-                <span>{repo.created_at}</span>
-                <br />
-                <span>{repo.description}</span>
-                <br />
-                <span>{repo.size} Mo</span>
-                <br />
-                <a href={repo.html_url}>Go to repo page </a>
-                <br />
-              </div>
-            </li>
-          ))}
+      <ul style={{ background: "#1f1f1f", padding: "2em" }}>
+        {reposLoaded &&
+          repos.map((repo) => {
+            const regex = /src="/gi;
+            const rmImgUpdated =
+              reposUpdated &&
+              repo.readme_html.replace(
+                regex,
+                `src="https://github.com/LeopoldAssogbaA/${repo.name}/raw/${repo.default_branch}/`
+              );
+
+            return (
+              <li key={repo.id}>
+                <div>
+                  <span>{repo.name}</span>
+                  <br />
+                  <span>{repo.created_at}</span>
+                  <br />
+                  <span>{repo.description}</span>
+                  <br />
+                  <span>{repo.size} Mo</span>
+                  <br />
+                  <a href={repo.html_url}>Go to repo page </a>
+                  <br />
+                  <section
+                    style={{ background: "darkgray", padding: "2em" }}
+                    dangerouslySetInnerHTML={{ __html: rmImgUpdated }}
+                  />
+                  <br />
+                </div>
+              </li>
+            );
+          })}
       </ul>
     </div>
   );
