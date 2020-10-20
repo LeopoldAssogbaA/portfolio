6f17e9c4bed7728f6a3f6043ddc58a1b42f77b1d
Léopold Assogba <leopoldassogba21@gmail.com>
2020-10-20 16:31:39 +0200

    finish dev page before content

--- src/components/dev/index.jsx
+++ src/components/dev/index.jsx
@@ -5,13 +5,16 @@ import { Col, Row } from "antd";
 import codeIcons from "./codeIcons";
 
 import "./index.less";
+import gsap from "gsap";
+import { Power2 } from "gsap/gsap-core";
+
 // TODO: handle lifecycle bug on page
 
 const DevComponent = () => {
+  // MatterJS
   const canvasRef = useRef(null);
   const boxRef = useRef(null);
   const [launched, setLauchend] = useState(false);
-
   useEffect(() => {
     if (!launched) {
       var Engine = Matter.Engine,
@@ -113,30 +116,183 @@ const DevComponent = () => {
       setLauchend(true);
     }
   }, [launched]);
-  // <Col span={4} ref={boxRef}>
-  //   <canvas ref={canvasRef} />
-  // </Col>
-  /* <div className="mockups">
-    <img
-      src="assets/mockups/webinar.png"
-      alt="current mockup"
-      className="img1"
-    />
-  </div> */
 
-  // Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+  // GSAP
+  let titleRevealRef = useRef();
+  let companyRevealRef = useRef();
+  let dateRevealRef = useRef();
+  let stackRevealRef = useRef();
+  let descriptionRevealRef = useRef();
+  useEffect(() => {
+    // title
+    gsap.to(titleRevealRef, {
+      duration: 0.5,
+      left: 0,
+      opacity: 1,
+    });
+    gsap.to(".title", {
+      duration: 0,
+      delay: 0.5,
+      opacity: 1,
+    });
+    gsap.to(titleRevealRef, {
+      duration: 0.5,
+      width: 0,
+      delay: 0.5,
+      ease: Power2.easeOut,
+    });
+    // company name
+    gsap.to(companyRevealRef, {
+      duration: 0.5,
+      left: 0,
+      opacity: 1,
+      delay: 0.2,
+    });
+    gsap.to(".companyName", {
+      duration: 0,
+      delay: 0.7,
+      opacity: 1,
+    });
+    gsap.to(companyRevealRef, {
+      duration: 0.5,
+      width: 0,
+      delay: 0.7,
+      ease: Power2.easeOut,
+    });
+    // company name
+    gsap.to(dateRevealRef, {
+      duration: 0.5,
+      left: 0,
+      opacity: 1,
+      delay: 0.4,
+    });
+    gsap.to(".date", {
+      duration: 0.5,
+      delay: 0.9,
+      opacity: 1,
+    });
+    gsap.to(dateRevealRef, {
+      duration: 0.5,
+      width: 0,
+      delay: 0.9,
+      ease: Power2.easeOut,
+    });
+    // stack
+    gsap.to(stackRevealRef, {
+      duration: 0.5,
+      left: 0,
+      opacity: 1,
+      delay: 0.6,
+    });
+    gsap.to(".stack", {
+      duration: 0,
+      delay: 1.1,
+      opacity: 1,
+    });
+    gsap.to(stackRevealRef, {
+      duration: 0.5,
+      width: 0,
+      delay: 1.1,
+      ease: Power2.easeOut,
+    });
+    // description
+    gsap.to(descriptionRevealRef, {
+      duration: 0.5,
+      left: 0,
+      opacity: 1,
+      delay: 0.8,
+    });
+    gsap.to(".description", {
+      duration: 0,
+      delay: 1.3,
+      opacity: 1,
+    });
+    gsap.to(descriptionRevealRef, {
+      duration: 0.5,
+      width: 0,
+      delay: 1.3,
+      ease: Power2.easeOut,
+    });
+
+    gsap.to(".mockupContainer", {
+      duration: 1,
+      opacity: 1,
+      delay: 1.8,
+    });
+    gsap.to(".devContainer", {
+      duration: 1,
+      opacity: 1,
+    });
+  });
 
   return (
     <div className="devContainer">
       <img src="assets/img/window.jpg" alt="window" className="window" />
       <Row>
-        <Col span={12} offset={2}>
+        <Col span={8} offset={2}>
           <div className="mockupContainer">
             <img
               src="assets/mockups/webinar.png"
               alt="current mockup"
               className="mockup"
             />
+            <div className="mockupReveal"></div>
+            <div className="box" ref={boxRef}>
+              <canvas ref={canvasRef} />
+            </div>
+          </div>
+        </Col>
+        <Col span={11} offset={1}>
+          <div className="infosContainer">
+            <div>
+              <div>
+                <div
+                  className="titleReveal"
+                  ref={(el) => (titleRevealRef = el)}
+                ></div>
+                <h2 className="title">Project Name</h2>
+              </div>
+              <div>
+                <div
+                  className="companyReveal"
+                  ref={(el) => (companyRevealRef = el)}
+                ></div>
+                <h3 className="companyName">Company Name</h3>
+              </div>
+              <div>
+                <div
+                  className="dateReveal"
+                  ref={(el) => (dateRevealRef = el)}
+                ></div>
+                <h3 className="date">20/2020</h3>
+              </div>
+              <div>
+                <div
+                  className="stackReveal"
+                  ref={(el) => (stackRevealRef = el)}
+                ></div>
+                <span className="stack">
+                  <b>Stack</b> Lorem ipsum dolor sit amet, consectetur
+                  adipiscing elit, sed do
+                </span>
+              </div>
+              <div>
+                <div
+                  className="descriptionReveal"
+                  ref={(el) => (descriptionRevealRef = el)}
+                ></div>
+                <p className="description">
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                  do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                  Ut enim ad minim veniam, quis nostrud exercitation ullamco
+                  laboris nisi ut aliquip ex ea commodo consequat. Duis aute
+                  irure dolor in reprehenderit in voluptate velit esse cillum
+                  dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+                  cupidatat non proident, sunt in culpa qui officia deserunt
+                  mollit anim id est laborum."
+                </p>
+              </div>
+            </div>
           </div>
         </Col>
       </Row>
